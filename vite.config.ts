51a4b346054d708fda4490067950576512c37b2c
lanrun <1947332325@qq.com>
2023-06-21 17:57:51 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,17 @@ export default defineConfig({
   resolve: {
     extensions: [".js", ".ts", ".tsx", ".jsx"],
   },
+  server: {
+    cors: true,
+    open: false,
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/map': {
+        target: 'http://localhost:8088/map',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/map/, '')
+      },
+    }
+  },
 })
